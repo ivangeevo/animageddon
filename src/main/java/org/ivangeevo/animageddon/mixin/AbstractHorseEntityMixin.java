aabbdd743ca8f0f4d3ac8891e5672cd7d8182102
Ivan Geevo <dbowgaming@gmail.com>
2024-12-29 00:34:34 +0200

    bump changes

---
+++ src/main/java/org/ivangeevo/animageddon/mixin/AbstractHorseEntityMixin.java
@@ -0,0 +1,76 @@
+package org.ivangeevo.animageddon.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.AbstractHorseEntity;
+import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractHorseEntity.class)
+public abstract class AbstractHorseEntityMixin extends AnimalEntity
+{
+
+    @Shadow protected abstract boolean getHorseFlag(int bitmask);
+
+    @Shadow protected abstract void setHorseFlag(int bitmask, boolean flag);
+
+    @Unique private int fedLevel = 0;
+
+    @Unique protected final int maxFedLevel = 480;
+
+    @Unique private final int rideableFedLevel = 400;
+
+    @Unique private final int untamedFedLevel = maxFedLevel / 16; // 3000
+
+
+    // holds the value of whether the horse is fed enough to be ridden
+    @Unique private static final int FED_FLAG = 128;
+
+
+    @Unique public boolean isFed() {
+        return this.getHorseFlag(FED_FLAG);
+    }
+
+    @Unique
+    public void setFed(boolean fed) {
+        this.setHorseFlag(FED_FLAG, fed);
+    }
+
+    protected AbstractHorseEntityMixin(EntityType<? extends AnimalEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    //@Inject(method = "tick", at = @At("TAIL"))
+    private void onTick(CallbackInfo ci) {
+        --fedLevel;
+        if (fedLevel < rideableFedLevel) {
+            this.stopRiding();
+        }
+    }
+
+    //@Inject(method = "tickMovement", at = @At("TAIL"))
+    private void onTickMovement(CallbackInfo ci) {
+        --fedLevel;
+        --fedLevel;
+    }
+
+    //@Inject(method = "receiveFood", at = @At(value = "INVOKE", target= "Lnet/minecraft/entity/passive/AbstractHorseEntity;isBaby()Z"))
+    private void onReceiveFood$beforeHeal(PlayerEntity player, ItemStack item, CallbackInfoReturnable<Boolean> cir, @Local float f) {
+        if (fedLevel < maxFedLevel) {
+            fedLevel += (int) f;
+        } else {
+            setFed(true);
+        }
+    }
+
+
+}
