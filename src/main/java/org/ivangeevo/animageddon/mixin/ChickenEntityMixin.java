95e3a3be0dbca8aa4f1d046df85d30f231b0c010
Ivan Geevo <dbowgaming@gmail.com>
2025-05-28 00:09:57 +0300

    made chickens to lay eggs only when fed chicken feed

--- src/main/java/org/ivangeevo/animageddon/mixin/ChickenEntityMixin.java
+++ src/main/java/org/ivangeevo/animageddon/mixin/ChickenEntityMixin.java
@@ -10,7 +10,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 import org.ivangeevo.animageddon.item.ModItems;
@@ -25,66 +28,72 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ChickenEntity.class)
 public abstract class ChickenEntityMixin extends AnimalEntity {
 
-    @Shadow public abstract boolean hasJockey();
-
     // Chicken related variables
     @Unique private boolean hasBeenFed = false;
     @Unique private long lastFedTime = -1;
+    @Unique private long lastWorldTime = -1;
     @Unique private long timeToLayEgg = 0;
 
-    @Unique private long lastWorldTime = -1;
+    @Shadow public int eggLayTime;
 
     protected ChickenEntityMixin(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
+        lastWorldTime = world.getTimeOfDay();
     }
 
-    @Override
-    public boolean getHasBeenFed() {
-        return hasBeenFed;
+    @Inject(method = "initGoals", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/ai/goal/GoalSelector;add(ILnet/minecraft/entity/ai/goal/Goal;)V", ordinal = 3))
+    private void modifyTemptGoal(CallbackInfo ci) {
+        // Set canBeScared to true
+        TemptGoal customTemptGoal =
+                new TemptGoal(this, 1.0, stack -> stack.isIn(BTWRConventionalTags.Items.CHICKEN_TEMPT_ITEMS), true);
+
+        this.goalSelector.add(3, customTemptGoal);
     }
 
-    @Override
-    public void setHasBeenFed(boolean value) {
-        hasBeenFed = value;
+    @Inject(method = "initGoals", at = @At("TAIL"))
+    private void addBreedingGoal(CallbackInfo ci) {
+        // Set canBeScared to false for breeding items
+        TemptGoal customBreedingGoal =
+                new TemptGoal(this, 1.25, stack -> stack.isOf(ModItems.CHICKEN_FEED), false);
+
+        this.goalSelector.add(3, customBreedingGoal);
     }
 
-    @Inject(method = "initGoals", at = @At("HEAD"), cancellable = true)
-    private void injectedInitGoals(CallbackInfo ci) {
-        this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(1, new EscapeDangerGoal(this, 2.0));
-        this.goalSelector.add(2, new AnimalMateGoal(this, 1.0));
-        this.goalSelector.add(3, new TemptGoal(this, 1.0, Ingredient.fromTag(BTWRConventionalTags.Items.CHICKEN_TEMPT_ITEMS), false));
-        this.goalSelector.add(3, new TemptGoal(this, 1.2, Ingredient.ofItems(ModItems.CHICKEN_FEED), false));
-
-        this.goalSelector.add(4, new FollowParentGoal(this, 1.1));
-        this.goalSelector.add(5, new WanderAroundFarGoal(this, 1.0));
-        this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
-        this.goalSelector.add(7, new LookAroundGoal(this));
-
-        ci.cancel();
-    }
-
-    //@Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/passive/ChickenEntity;getWorld()Lnet/minecraft/world/World;"), cancellable = true)
+    @Inject(method = "tickMovement", at = @At("TAIL"))
     private void onTickMovement(CallbackInfo ci) {
+        // set the original egg lay time to max int value to practically make it never reach 0
+        this.eggLayTime = Integer.MAX_VALUE;
 
-        long worldTime = getWorld().getTimeOfDay() % 24000;
-
-        // Check if it's sunrise
-        if (worldTime < lastWorldTime) {
-
-            if (this.getHasBeenFed() && !this.getWorld().isClient() && this.isAlive() && !this.isBaby() && !this.hasJockey()) {
-                // The chicken has been fed, lay an egg
-                this.playSound(SoundEvents.ENTITY_CHICKEN_EGG, 1.0F,
-                        (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
+        if (!this.isBaby() /**&& isFullyFed()**/ && timeToLayEgg > 0 && validateTimeToLayEgg(this.getWorld())) {
+            if (this.getWorld().getTimeOfDay() > timeToLayEgg) {
+                this.playSound(SoundEvents.ENTITY_SLIME_ATTACK,1.0f, this.getSoundPitch());
+                this.playSound(SoundEvents.ENTITY_CHICKEN_HURT,1.0f, this.getSoundPitch());
                 this.dropItem(Items.EGG);
-                this.emitGameEvent(GameEvent.ENTITY_PLACE);
-                this.setHasBeenFed(false);
+                this.timeToLayEgg = 0;
+
             }
         }
+    }
 
-        lastWorldTime = worldTime;
+    @Override
+    protected void eat(PlayerEntity player, Hand hand, ItemStack stack) {
+        if (stack.isOf(ModItems.CHICKEN_FEED) && !isBaby()) {
+            long currentTime = this.getWorld().getTimeOfDay();
 
-        ci.cancel();
+            // following morning, at least half day from now
+            timeToLayEgg = (((currentTime + 12000L) / 24000L) + 1) * 24000L;
+
+            // crack of dawn (22550) + 30 seconds random variance
+            timeToLayEgg += -1450 + this.random.nextInt(600);
+            this.playSound(SoundEvents.ENTITY_CHICKEN_HURT, this.getSoundVolume(), this.random.nextFloat() * 0.2F + 1.5F);
+        }
+        super.eat(player, hand, stack);
+    }
+
+    @Inject(method = "isBreedingItem", at = @At("HEAD"), cancellable = true)
+    private void onIsBreedingItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+        //cir.setReturnValue(stack.isOf(ModItems.CHICKEN_FEED));
+        cir.setReturnValue(false);
     }
 
     @Inject(method = "writeCustomDataToNbt", at = @At("TAIL"))
@@ -107,9 +116,27 @@ public abstract class ChickenEntityMixin extends AnimalEntity {
         }
     }
 
-    @Inject(method = "isBreedingItem", at = @At("HEAD"), cancellable = true)
-    private void onIsBreedingItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        cir.setReturnValue(stack.isOf(ModItems.CHICKEN_FEED));
+    @Unique
+    private boolean validateTimeToLayEgg(World world) {
+        long currentTime = world.getTime();
+        long deltaTime = timeToLayEgg - currentTime;
+
+        if (deltaTime > 48000L) {
+            // we're more than 2 days before the time, something is wrong (like a time change command), so don't lay
+            timeToLayEgg = 0;
+
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean getHasBeenFed() {
+        return hasBeenFed;
+    }
+
+    public void setHasBeenFed(boolean value) {
+        hasBeenFed = value;
     }
 
 
